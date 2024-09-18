3efba4c8f1b1666d1d064bdb915009975deb6566
ajayakash2608 <130428831+ajayakash2608@users.noreply.github.com>
2024-09-18 16:29:56 +0530

    Update Redirect.js

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -6,35 +6,17 @@ const Redirect = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchLongUrl = async () => {
-      try {
-        // Fetch the URL data from the backend using the short URL
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/urls/${shortUrl}`);
-        if (response.ok) {
-          const urlData = await response.json();
-          
-          // If URL data is found, redirect to the long URL
-          if (urlData && urlData.longUrl) {
-            window.location.href = urlData.longUrl;
-          } else {
-            // If URL not found, navigate to the homepage
-            navigate('/');
-          }
-        } else {
-          // Handle case where the response is not ok (e.g., 404)
-          navigate('/');
-        }
-      } catch (error) {
-        console.error('Error fetching the URL:', error);
-        navigate('/');
-      }
-    };
+    const urls = JSON.parse(localStorage.getItem('urls')) || [];
+    const urlData = urls.find(url => url.shortUrl === shortUrl);
 
-    fetchLongUrl();
+    if (urlData) {
+      window.location.href = urlData.longUrl;
+    } else {
+      navigate('/');
+    }
   }, [shortUrl, navigate]);
 
   return <div>Redirecting...</div>;
 };
 
 export default Redirect;
-
